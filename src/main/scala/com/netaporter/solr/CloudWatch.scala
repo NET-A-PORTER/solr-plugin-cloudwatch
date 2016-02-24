9cd11170da9087d0a11d3820d62333e0606316ea
Ian Forsey <ian.forsey@net-a-porter.com>
2016-02-24 17:18:40 +0000

    Add configuration for either host specific metrics to be in a different namespace or the same namespace with a host dimension

--- src/main/scala/com/netaporter/solr/CloudWatch.scala
+++ src/main/scala/com/netaporter/solr/CloudWatch.scala
@@ -3,6 +3,8 @@ package com.netaporter.solr
 import com.amazonaws.metrics.AwsSdkMetrics
 import org.slf4j.LoggerFactory
 
+import scala.util.Try
+
 object CloudWatch {
 
   val log = LoggerFactory.getLogger(getClass)
@@ -11,16 +13,24 @@ object CloudWatch {
   lazy val metricsEnabled = init()
   def ensureMetricsSetup(): Boolean = metricsEnabled
 
+  def sysPropBoolean(name: String, default: Boolean) =
+    sys.props.get(name)
+      .flatMap(strValue => Try(strValue.toBoolean).toOption)
+      .getOrElse(default)
+
   private def init() = {
     log.info("Initialising CloudWatchMetrics Plugin")
 
     val hostMetricName = sys.props.get("awsmetrics.host-metric-name")
+    val singleMetricNamespace = sysPropBoolean("awsmetrics.single-metric-namespace", default = true)
+    val perHostMetricsIncluded = sysPropBoolean("awsmetrics.per-host-metrics-included", default = true)
 
     if(hostMetricName.isDefined) {
       AwsSdkMetrics.setHostMetricName(hostMetricName.get)
     }
 
-    AwsSdkMetrics.setPerHostMetricsIncluded(true)
+    AwsSdkMetrics.setSingleMetricNamespace(singleMetricNamespace)
+    AwsSdkMetrics.setPerHostMetricsIncluded(perHostMetricsIncluded)
 
     AwsSdkMetrics.enableDefaultMetrics()
   }
